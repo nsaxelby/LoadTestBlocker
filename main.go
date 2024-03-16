b44dc108353c35f483d10e6dc270f17cc3a7e7d8
nsaxelby <mail@nsaxelby.com>
2024-03-16 17:43:31 +0000

    Added basic WS broadcast with loop

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+func loop(hub *Hub) {
+	var tick = 0
+	for {
+		log.Println("tick", tick)
+		hub.broadcast <- []byte(strconv.Itoa(tick))
+		tick++
+		time.Sleep(time.Second)
+	}
+}
 
 func main() {
-	fmt.Println("hello world")
+	hub := newHub()
+	go hub.run()
+
+	fs := http.FileServer(http.Dir("./web"))
+	// Start web server to show basic info, just a text box is fine
+	http.Handle("/", fs)
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+
+	server := &http.Server{
+		Addr:              *addr,
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+
+	go loop(hub)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+	// Setup signalr, or some sort of web socket library to stream results to ther web site, just a console/log is fine for now
 }
