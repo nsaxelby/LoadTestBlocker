c20c4e9fbb2959db418bb61b22e215bb7233d159
nsaxelby <mail@nsaxelby.com>
2024-03-22 18:57:13 +0000

    Small refactor - readability

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,11 +13,17 @@ import (
 var addr = flag.String("addr", ":8080", "http service address")
 
 func main() {
+	// Hub manages the websocket connections on the website
 	hub := website.NewHub()
 	go hub.Run()
 
+	// LoadTestManager is what does the heartbeat, and the actual load test
 	manager := loadtest.NewLoadTestManager(hub)
 
+	initWebsite(hub, manager)
+}
+
+func initWebsite(hub *website.Hub, manager *loadtest.LoadTestManager) {
 	fs := http.FileServer(http.Dir("./web"))
 
 	// Start web server to show basic info, just a text box is fine
